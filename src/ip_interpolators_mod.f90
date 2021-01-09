3915f04768f46471bc7cb60e20ba42498eb712ef
kgerheiser <kgerheiser@icloud.com>
2021-01-08 23:20:49 -0500

    Make integer constants for interpolation options

--- src/ip_interpolators_mod.f90
+++ src/ip_interpolators_mod.f90
@@ -8,5 +8,12 @@ module ip_interpolators_mod
   use neighbor_budget_interpolator_scalar_mod
 
   implicit none
+
+  integer, parameter, public :: BILINEAR_INTERP_ID = 0
+  integer, parameter, public :: BICUBIC_INTERP_ID = 1
+  integer, parameter, public :: NEIGHBOR_INTERP_ID = 2
+  integer, parameter, public :: BUDGET_INTERP_ID = 3
+  integer, parameter, public :: SPECTRAL_INTERP_ID = 4
+  integer, parameter, public :: NEIGHBOR_BUDGET_INTERP_ID = 6
 end module ip_interpolators_mod
 
